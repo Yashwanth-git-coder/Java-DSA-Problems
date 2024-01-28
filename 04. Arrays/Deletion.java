ab574ef833eb7bf589f9666c594dfc5240c62026
Yashwanth S <yashwanth7780@gmail.com>
2024-01-28 11:56:20 +0530

    Deletion Opretion

---
+++ 04. Arrays/Deletion.java
@@ -0,0 +1,34 @@
+public class Deletion {
+
+    static int DeleteEle(int arr[], int n, int x){
+        int i = 0;
+        for(i = 0 ; i < n ; i++){
+            if(arr[i] == x)
+                break;
+        }
+        if(i == n){
+            return n;
+        }
+        for(int j = i ; j < n - 1 ; j++){
+            arr[j] = arr[j + 1];
+        }
+        return n - 1;
+    }
+    public static void main(String[] args){
+        int arr[] = {5,6,7,8,9}, x = 6, n = 5;
+
+        System.out.println("Before Deletion : ");
+
+        for(int i = 0 ; i < n ; i++){
+            System.out.println(arr[i]);
+        }
+
+        System.out.println("After Deletion : ");
+
+        n = DeleteEle(arr, n, x);
+
+        for(int i = 0 ; i < n ; i++){
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
