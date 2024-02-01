743982b65f41c5ce4ef2b5a112207f7f6ea9cfde
Yashwanth S <yashwanth7780@gmail.com>
2024-02-01 10:23:24 +0530

    Left shifting an array

---
+++ 04. Arrays/Left_Rotate_an_Array.java
@@ -0,0 +1,32 @@
+public class Left_Rotate_an_Array {
+
+    static void leftRotate(int a[], int n) {
+        int temp = a[0];
+
+        for (int i = 1; i < n; i++) {
+            a[i - 1] = a[i];
+        }
+        a[n - 1] = temp;
+    }
+
+    public static void main(String[] args) {
+        int a[] = { 2, 3, 5, 8, 9, 8, 6, 7, 6 };
+        int n = a.length;
+
+        System.out.println("Before Modification :");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i]);
+        }
+        System.out.println(" ");
+
+        // Perform left rotation by one
+        leftRotate(a, n);
+
+        System.out.println("After Modification :");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i]);
+        }
+    }
+}
