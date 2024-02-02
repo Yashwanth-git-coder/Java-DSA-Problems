81ce2bae4fea2afbe4ead8424d7336729e0406e4
Yashwanth S <yashwanth7780@gmail.com>
2024-02-02 10:19:57 +0530

    solution

--- 04. Arrays/Leaders_in_Array.java
+++ 04. Arrays/Leaders_in_Array.java
@@ -1,3 +1,20 @@
 public class Leaders_in_Array {
-    
+
+    void PrintLeaders(int arr[], int n){
+        int max_from_right = arr[n - 1];
+        System.out.print(max_from_right + " ");
+
+        for(int i = n - 2 ; i > 0 ; i--){
+            if(max_from_right < arr[i]){
+                max_from_right = arr[i];
+                System.out.print(max_from_right + " ");
+            }
+        }
+    }
+    public static void main(String[] args){
+        Leaders_in_Array lead = new Leaders_in_Array();
+        int arr[] = new int[] { 16, 17, 4, 3, 5, 2 };
+        int n = arr.length;
+        lead.PrintLeaders(arr, n);
+    }
 }
