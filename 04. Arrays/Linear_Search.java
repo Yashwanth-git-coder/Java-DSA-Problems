89fa21fd1a12227dc0613ea37804e1afd2b7f2ca
Yashwanth <yashwanth7780@gmail.com>
2024-05-19 22:50:59 +0530

    Linear Search

---
+++ 04. Arrays/Linear_Search.java
@@ -0,0 +1,22 @@
+public class Linear_Search {
+    public static int findElement(int marks[], int key){
+        for(int i = 0; i < marks.length; i++){
+            if(marks[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int marks[] = {23, 34, 56, 76};
+        int key = 34;
+
+        int index = findElement(marks, key);
+
+        if(index == -1){
+            System.out.println("Not Found");
+        }else{
+            System.out.println("Key is at Index" + " " + index);
+        }
+    }
+}
