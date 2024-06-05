46fb5966707557fa8dac5bbfd7837d821cd29a03
Yashwanth <yashwanth7780@gmail.com>
2024-06-05 19:10:59 +0530

    Concetination

---
+++ 08. Strings/D_Concatenation.java
@@ -0,0 +1,9 @@
+public class D_Concatenation {
+    public static void main(String[] args){
+        String firstname = "Yashwanth";
+        String lastname = "Shivaraju";
+        String name = firstname + " " + lastname;
+        System.out.println("This is my name " + name);
+    }
+}
+
