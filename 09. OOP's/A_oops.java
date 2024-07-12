596b9bc016d0ed39555a773ff03c6aef031efa7f
Yashwanth <yashwanth7780@gmail.com>
2024-07-12 19:14:22 +0530

    Constructors

--- 09. OOP's/A_oops.java
+++ 09. OOP's/A_oops.java
@@ -3,27 +3,49 @@ public class A_oops {
         Pen p1 = new Pen();
 
         p1.SetColor("Blue");
-        System.out.println(p1.Color);
+        System.out.println(p1.getColor());
 
         p1.SetTip(5);
-        System.out.println(p1.Tip);
+        System.out.println(p1.getTip());
 
         p1.SetColor("Yellow");
-        System.out.println(p1.Color);
+        System.out.println(p1.getColor());
+
+        // BankAccount myacc = new BankAccount();
+        // myacc.username = "Yashwanth";
+        // myacc.setPassword("asdfgh");
+
 
     }
 }
+
+// class BankAccount {
+//     public String username;
+//     private String password;
+
+//     public void setPassword(String pwd){
+//         password = pwd;
+//     }
+// }
 
 class Pen{
 
     String Color;
     int Tip;
 
+    String getColor(){
+        return this.Color;
+    }
+
+    String getTip() {
+        return this.Color;
+    }
+
     void SetColor(String newcolor){
-        Color = newcolor;
+        this.Color = newcolor;
     }
 
     void SetTip(int newtip){
-        Tip = newtip;
+        this.Tip = newtip;
     }
 }
