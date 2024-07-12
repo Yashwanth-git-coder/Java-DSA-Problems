623dd0d7e40c6dc4222e8ab26ebcf443e304e0b3
Yashwanth <yashwanth7780@gmail.com>
2024-07-12 19:17:57 +0530

    Constructor

--- 09. OOP's/B_constructors.java
+++ 09. OOP's/B_constructors.java
@@ -1,3 +1,17 @@
+import java.sql.Struct;
+
 public class B_constructors {
-    
+    public static void main(String[] args){
+        Student s1 = new Student("Yashwanth");
+        System.out.println(s1.name);
+    }
+}
+
+class Student {
+    String name;
+    int roll;
+
+    Student(String name){
+        this.name = name;
+    }
 }
