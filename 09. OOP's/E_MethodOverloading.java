a097c3163fa2e868e737e14565f9a5e8e567e835
Yashwanth <yashwanth7780@gmail.com>
2024-07-17 20:23:06 +0530

    Update

--- 09. OOP's/E_MethodOverloading.java
+++ 09. OOP's/E_MethodOverloading.java
@@ -5,9 +5,28 @@ public class E_MethodOverloading {
         System.out.println(add.sum(2, 4));
         System.out.println(add.sum((float) 1.3, (float) 3.4));
         System.out.println(add.sum(2, 4, 6));
+
+        Deer d = new Deer();
+
+        d.eats();
     }
 }
 
+// Methos Overriding
+class Animal {
+    void eats() {
+        System.out.println("Eats Anything");
+    }
+}
+
+
+class Deer extends Animal {
+    void eats() {
+        System.out.println("Eats Grass");
+    }
+}
+
+// Method Overloading
 class Calclu {
     int sum;
 
